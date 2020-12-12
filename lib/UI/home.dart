432f62eb0803a8526e399f70b280990d9b895a17
Mohamed Minsaaf <minsafmohamed7@gmail.com>
2020-12-12 19:45:44 +0530

    added home screen

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -8,35 +8,28 @@ class Home extends StatefulWidget {
 int index = 0;
 
 Widget show(int index) {
-  switch (index) {
-    case 0:
-      return netflixHome();
-      break;
-    case 1:
-      return netflixSearch();
-      break;
-    case 2:
-      return netflixDownload();
-      break;
-    case 3:
-      return netflixMore();
-      break;
-  }
+  return NetflixHome();
+  // switch (index) {
+  //   case 0:
+  //     return netflixHome();
+  //     break;
+  //   case 1:
+  //     return netflixSearch();
+  //     break;
+  //   case 2:
+  //     return netflixDownload();
+  //     break;
+  //   case 3:
+  //     return netflixMore();
+  //     break;
+  // }
 }
-
-Widget netflixHome() {}
-
-Widget netflixSearch() {}
-
-Widget netflixDownload() {}
-
-Widget netflixMore() {}
 
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: show(index),
+      body: show(index),
       backgroundColor: Color(0xFF221f1f),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
@@ -75,11 +68,90 @@ class _HomeState extends State<Home> {
               //  backgroundColor: Colors.black,
               backgroundColor: Color(0xFF221f1f),
               title: Text(
-                "Moew",
+                "More",
                 style: TextStyle(color: Colors.white),
               ))
         ],
         backgroundColor: Colors.black87,
+      ),
+    );
+  }
+}
+
+//Homescreen
+class NetflixHome extends StatefulWidget {
+  @override
+  _NetflixHomeState createState() => _NetflixHomeState();
+}
+
+class _NetflixHomeState extends State<NetflixHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF221f1f),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 1.5,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Container(
+                child: Image(
+                    image: NetworkImage(
+                        'https://imageresizer.static9.net.au/u8vej4niCqfChLZIQZVuN5OoyPo=/396x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2Fab97f8bd-9966-4c8c-83e3-40f35d076423')),
+              ),
+
+//container for the image gradient
+              Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    gradient: LinearGradient(
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter,
+                        colors: [
+                          Color(0xFF221f1f).withOpacity(0.0),
+                          Color(0xFF221f1f)
+                        ])),
+              ),
+
+              Positioned(
+                  left: 10,
+                  top: 30,
+                  child: Container(
+                    height: 70,
+                    width: 100,
+                    child: Image.network(
+                        'https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/64-netflix-256.png'),
+                  )),
+
+              Positioned(
+                  left: 100,
+                  top: 55,
+                  child: Text(
+                    "Series",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  )),
+
+              Positioned(
+                  left: 200,
+                  top: 55,
+                  child: Text(
+                    "Film",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  )),
+              Positioned(
+                  left: 300,
+                  top: 55,
+                  child: Text(
+                    "My List",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  )),
+            ],
+          )
+        ],
       ),
     );
   }
